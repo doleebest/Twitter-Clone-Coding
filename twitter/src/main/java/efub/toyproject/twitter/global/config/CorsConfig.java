8ab3c7949f64500c4f79b1da4f32a407cc882b09
So Jeong Lee <sj27419@gmail.com>
2023-05-24 16:10:35 +0900

    add global

---
+++ twitter/src/main/java/efub/toyproject/twitter/global/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package efub.toyproject.twitter.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter(){
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+
+        // 프론트엔드의 도메인이 들어가는 파트
+        // 미리 짝개발자에게 이야기해, 배포된 도메인을 받아 새로 도메인을 allow
+        // 만약 "https://abc.dkfj"라는 도메인을 전달받았다면, 아래 코드에 추가로 다음과 같이 입력
+        // config.addAllowedOrigin("https://abc.dkfj/");
+        config.addAllowedOrigin("http://localhost:3000/");
+
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        config.setMaxAge(3600L);
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
