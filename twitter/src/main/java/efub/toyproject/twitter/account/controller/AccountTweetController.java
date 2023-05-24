717e780d093690ad2ea3e4ef65209d659acbd185
So Jeong Lee <sj27419@gmail.com>
2023-05-24 14:53:49 +0900

    add account 기능

--- twitter/src/main/java/efub/toyproject/twitter/account/controller/AccountTweetController.java
+++ twitter/src/main/java/efub/toyproject/twitter/account/controller/AccountTweetController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 // url: /accounts/{accountId}/tweets
-
+//
 @RestController
 @RequestMapping("/accounts/{accountId}/tweets")
 @RequiredArgsConstructor
