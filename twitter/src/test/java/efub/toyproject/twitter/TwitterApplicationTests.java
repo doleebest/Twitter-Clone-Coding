b2e22123ebfcf58892d49876878253964006573f
So Jeong Lee <sj27419@gmail.com>
2023-05-27 18:21:32 +0900

    fix TwitterApplicationTests annotation 제거

--- twitter/src/test/java/efub/toyproject/twitter/TwitterApplicationTests.java
+++ twitter/src/test/java/efub/toyproject/twitter/TwitterApplicationTests.java
@@ -3,10 +3,10 @@ package efub.toyproject.twitter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TwitterApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
